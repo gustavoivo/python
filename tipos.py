142aefca4a793f94998d985647f2565e0c0e68df
gucarvalho <gucarvalho@uolinc.com>
2019-10-13 21:09:35 -0300

    python

---
+++ tipos.py
@@ -0,0 +1,23 @@
+# Comentario
+"""Comentario em duas linhas
+Duas linhas"""
+#Criar variavel
+#nome = valor
+print ("Meu Primeiro codigo em Python\n")  #\n para pular a linha
+#nome_da_variavel = "Valor_da_variavel"
+#String
+Nome = "Gustavo"
+Sobrenome = "Ivo"
+#print (Nome + " " + Sobrenome)
+print (Nome, Sobrenome)
+
+soma = 3 + 3
+#subitratir = 3 -3
+#divisao = 3 /3
+igual = 3==3
+diferente =  3 !=2
+print (diferente)
+print (igual)
+print ("soma")
+print ( type(diferente) )
+
