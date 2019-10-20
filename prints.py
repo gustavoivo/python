ed7696681a388a3e5d11ad273ab120a883e8734d
gucarvalho <gucarvalho@uolinc.com>
2019-10-20 20:59:13 -0300

    Aula2

---
+++ prints.py
@@ -0,0 +1,10 @@
+time1 = "Santos"
+time2 = "Palmeiras"
+print(time1, time2)
+print(time1 + time2)
+
+#python2
+print("O %s Nao tem mundial!!!" %time2)
+
+#python3
+print( "O {} Nao tem mundial" .format (time2))
